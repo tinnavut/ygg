5aa129c59fbb728953814c03e718cfaee8c5e4cd
tinnavut <116094167+tinnavut@users.noreply.github.com>
2022-10-18 21:55:49 +0700

    Add files via upload

---
+++ admin/data.php
@@ -0,0 +1,35 @@
+<?php include('h.php');?>
+<body class="hold-transition skin-purple sidebar-mini">
+  <div class="wrapper">
+    <!-- Main Header -->
+    <?php include('menutop.php');?>
+    <!-- Left side column. contains the logo and sidebar -->
+    
+        <?php include('menu_l.php');?>
+      
+    <div class="content-wrapper">
+      <section class="content">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="box">
+              <div class="row">
+                <div class="col-sm-12">
+                  <div class="box-body">
+                    <?php
+                  $act = (isset($_GET['act']) ? $_GET['act'] : '');
+                  if($act == 'edit'){
+                  include('order_form_edit.php');
+                  }else {
+                  include('data_list.php');
+                  }
+                  ?>                   
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </body>
+  </html>
+  <?php include('footerjs.php');?>
---
+++ admin/data_list.php
@@ -0,0 +1,56 @@
+<?php 
+ if(@$_GET['do']=='success'){
+    echo '<script type="text/javascript">
+          swal("", "ทำรายการสำเร็จ !!", "success");
+          </script>';
+    echo '<meta http-equiv="refresh" content="1;url=product.php" />';
+
+  }else if(@$_GET['do']=='finish'){
+    echo '<script type="text/javascript">
+          swal("", "แก้ไขสำเร็จ !!", "success");
+          </script>';
+    echo '<meta http-equiv="refresh" content="1;url=product.php" />';
+  }
+
+$query = "SELECT * FROM tbl_data as k
+INNER JOIN tbl_type as t ON p.type_id = t.type_id
+ORDER BY p.p_id DESC" or die("Error:" . mysqli_error());
+$result = mysqli_query($con, $query);
+echo ' <table id="example1" class="table table-bordered table-striped">';
+  echo "<thead>";
+    echo "<tr class=''>
+      <th width='3%'  class='hidden-xs'>ID</th>
+      <th width='8%' class='hidden-xs'>รูป</th>
+      <th width='10%'>รหัสสินค้า</th>
+       <th width='20%'>ชื่อ</th>
+       <th width='30%' class='hidden-xs'>ที่อยู่</th>
+      <th>ราคาสินค้า</th>
+       <th>จำนวนเข้าชม</th>
+      <th width='7%'>-</th>
+    </tr>";
+  echo "</thead>";
+  while($row = mysqli_fetch_array($result)) {
+  echo "<tr>";
+  echo "<td  class='hidden-xs'>" .$row["p_id"] .  "</td> ";
+    echo "<td  class='hidden-xs'>" .$row["id_name"] .  "</td> ";
+    echo "<td class='hidden-xs'>"."<img src='../p_img/".$row['k_img']."' width='100%'>"."</td>";
+    echo "<td>" .$row["kn_id"] .  "</td> ";
+    echo "<td> ชื่อ: " .$row["name"] .
+    "<br>ประเภท: <font color='blue'>".$row["type_name"] ."</font>".
+      "</td class='hidden-xs'> ";
+    echo "<td class='hidden-xs'>" .$row["k_detail"] ."</td> ";
+       echo "<td> ราคา " .$row["k_price"] ." บาท".    "<br>จำนวน ".$row["k_qty"]." ".$row["k_unit"].
+      "</td> ";
+      "</td> ";
+      echo "<td> จำนวนการเข้าชม " .$row["p_view"] ." ครั้ง".
+    "<br>วันที่ ".date('d/m/Y',strtotime($row["datesave"])).
+
+      "</td> ";
+        echo "<td><a href='data.php?act=edit&ID=$row[p_id]' class='btn btn-warning btn-xs'><span class='glyphicon glyphicon-edit'></span></a> 
+        <a href='data_del_db.php?ID=$row[p_id]' onclick=\"return confirm('ยันยันการลบ')\" class='btn btn-danger btn-xs'><span class='glyphicon glyphicon-trash'></span></a>        
+    </td> ";
+    
+  }
+echo "</table>";
+mysqli_close($con);
+?>
